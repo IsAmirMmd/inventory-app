3937d3fa2779a635286dad787a99382ca79161f6
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-07 18:42:36 +0330

    categry form modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import CategoryForm from "./components/CategoryForm";
 import Navbar from "./components/NavBar";
 
 function App() {
@@ -6,6 +7,9 @@ function App() {
     <div className="App">
       <div className="bg-slate-800 min-h-full">
         <Navbar />
+        <div className="max-w-screen-sm container mx-auto p-2">
+          <CategoryForm />
+        </div>
       </div>
     </div>
   );
--- src/components/CategoryForm.jsx
+++ src/components/CategoryForm.jsx
@@ -1,5 +1,58 @@
 const CategoryForm = () => {
-    return (  );
-}
- 
+  return (
+    <>
+      <div className="mb-4 hidden" id="category-container">
+        <h2 className="text-xl text-slate-300 font-bold mb-2">
+          Add new category
+        </h2>
+        <form className="bg-slate-700 rounded-xl flex flex-col gap-y-4 p-4">
+          <div>
+            <label for="title" className="block text-slate-400">
+              category title
+            </label>
+            <input
+              className="bg-transparent rounded-md p-1 border border-slate-500 outline-0 text-slate-300"
+              type="text"
+              id="title"
+              name="title"
+            />
+          </div>
+          <div>
+            <label className="block text-slate-400" for="description">
+              description
+            </label>
+            <textarea
+              className="bg-transparent rounded-md p-1 border border-slate-500 outline-0 text-slate-300 w-full"
+              type="text"
+              id="description"
+              name="description"
+            ></textarea>
+          </div>
+          <div className="flex justify-between flex-grow gap-x-2">
+            <button
+              className="py-2 border border-slate-500 rounded-lg flex-grow text-slate-400"
+              id="cancelFromAdd"
+              type="reset"
+            >
+              cancel
+            </button>
+            <button
+              id="addToCategory"
+              className="rounded-lg py-2 flex-grow bg-slate-500 text-slate-200"
+            >
+              add
+            </button>
+          </div>
+        </form>
+      </div>
+      <button
+        id="toggle-category"
+        className="outline-none border-0 text-slate-400"
+      >
+        Add new category
+      </button>
+    </>
+  );
+};
+
 export default CategoryForm;
