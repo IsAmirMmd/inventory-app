51d5ee70eaaaff9c19f9b5b3e25886b960106fed
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-07 18:34:46 +0330

    navbar completed for app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="bg-slate-800 min-h-full">
+        <Navbar />
+      </div>
     </div>
   );
 }
