87bbf9fc83a112c7d96af8dbc847fbac68a43d5c
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-07 18:30:33 +0330

    category and products added as sample

---
+++ src/components/category.jsx
@@ -0,0 +1,14 @@
+export const Categories = [
+  {
+    id: 1,
+    title: "phone",
+    description: "lorem ipsum for some.",
+    date: "2023-03-17T14:25:55.143Z",
+  },
+  {
+    id: 2,
+    title: "laptop",
+    description: "lorem ipsum for some.",
+    date: "2023-02-27T14:25:55.143Z",
+  },
+];
---
+++ src/components/products.jsx
@@ -0,0 +1,16 @@
+export const Products = [
+  {
+    id: 1,
+    title: "iPhone 13",
+    quantity: 10,
+    Category: "phone",
+    date: "2023-03-17T14:25:55.143Z",
+  },
+  {
+    id: 2,
+    title: "MacBook M1 Pro",
+    quantity: 5,
+    Category: "laptop",
+    date: "2023-03-17T14:25:55.143Z",
+  },
+];
