046da9537f49a2a236558d85dd3747d7ce65dd17
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-08 15:26:34 +0330

    attaching product form to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import CategoryForm from "./components/CategoryForm";
 import Navbar from "./components/NavBar";
+import ProductForm from "./components/ProductForm";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
         <div className="max-w-screen-sm container mx-auto p-2">
           <CategoryForm />
+          <ProductForm />
         </div>
       </div>
     </div>
